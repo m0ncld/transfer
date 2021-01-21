9e28131c05933ef52c869408334a768f7591cf25
Łukasz Łuszczek <luk.luszczek@gmail.com>
2021-01-21 20:36:37 +0100

    Initial PMD and Checkstyle configuration

--- src/main/java/com/challenge/transfer/Application.java
+++ src/main/java/com/challenge/transfer/Application.java
@@ -1,16 +1,21 @@
 package com.challenge.transfer;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
-
+/**
+ * Main Spring Application class.
+ */
 @SpringBootApplication
 public class Application {
 
+    private Application() { }
+
+    /**
+     * Main system method.
+     *
+     * @param args System arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
